9493eff98a9858156014adcdb2567fa206aebfe6
stefanobellomo <stefanobellomo1998@gmail.com>
2025-11-24 11:58:25 +0100

    create array and 5 obj and create app.get for bacheca

--- server.js
+++ server.js
@@ -7,5 +7,41 @@ app.listen(PORT, () => {
 })
 
 app.get('/', (req, res) => {
+    res.send('Server del mio blog')
+    const posts = [
+        {
+            titolo: '',
+            contenuto: '',
+            img: '',
+            tags: ['']
+        },
+        {
+            titolo: '',
+            contenuto: '',
+            img: '',
+            tags: ['']
+        },
+        {
+            titolo: '',
+            contenuto: '',
+            img: '',
+            tags: ['']
+        },
+        {
+            titolo: '',
+            contenuto: '',
+            img: '',
+            tags: ['']
+        },
+        {
+            titolo: '',
+            contenuto: '',
+            img: '',
+            tags: ['']
+        }
+    ]
+})
+
+app.get('/bacheca', (req, res) => {
 
 })
